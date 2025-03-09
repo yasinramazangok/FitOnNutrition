46f93fd49e90643daaae5bce94c828a1f7809558
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-09 22:30:48 +0300

    Project has been created. Services/Catalog/MultiShop.Catalog Dtos, Entities, Mapping, Settings and Category service have been added.

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
+            _mapper = mapper;
+        }
+
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetListAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
