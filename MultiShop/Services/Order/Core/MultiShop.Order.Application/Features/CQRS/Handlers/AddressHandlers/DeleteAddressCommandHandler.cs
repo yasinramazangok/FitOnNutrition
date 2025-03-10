bf4503d7ef78f96b6a46f4c7c073392d1ed3ddf1
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-11 01:35:02 +0300

    Services/Order/Core CQRS, Services/Order/Infrastructure Context and Repository, Services/Order/Presentation Address and OrderDetail controllers have been added.

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/DeleteAddressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class DeleteAddressCommandHandler
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public DeleteAddressCommandHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public async Task Handle(DeleteAddressCommand deleteAddressCommand)
+        {
+            var value = await _addressRepository.GetByIdAsync(deleteAddressCommand.Id);
+            await _addressRepository.DeleteAsync(value);
+        }
+    }
+}
---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/DeleteOrderDetailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class DeleteOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _orderDetailRepository;
+
+        public DeleteOrderDetailCommandHandler(IRepository<OrderDetail> orderDetailRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+        }
+
+        public async Task Handle(DeleteOrderDetailCommand command)
+        {
+            var value = await _orderDetailRepository.GetByIdAsync(command.Id);
+            await _orderDetailRepository.DeleteAsync(value);
+        }
+    }
+}
