94d65fc8b5fa3048aa6c71adb3a4066dec2a0026
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-10 20:39:50 +0300

    Services/Catalog/MultiShop.Catalog Category, Product, ProductDetail and ProductImage API Controllers have been added.

--- MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
@@ -4,7 +4,7 @@
     {
         public string ProductDetailId { get; set; }
         public string ProductDescription { get; set; }
-        public string ProductInfo { get; set; }
+        public string ProductInformation { get; set; }
         public string ProductId { get; set; }
     }
 }
