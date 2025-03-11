2a47d831bb883d627b4545c99d44313a5eadb2d6
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-11 14:37:18 +0300

    Services/Order/Core Mediator design pattern has been added.

--- MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderCommands/CreateOrderCommand.cs
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Commands/OrderCommands/CreateOrderCommand.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultiShop.Order.Domain.Entities
+namespace MultiShop.Order.Application.Features.Mediator.Commands.OrderCommands
 {
-    public class Order
+    public class CreateOrderCommand : IRequest
     {
-        public int OrderId { get; set; }
         public string UserId { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
-        public List<OrderDetail> OrderDetails { get; set; }
     }
 }
