88a3c756d74be7648c0ab75fe7b0a29d527f8d6c
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-16 02:10:55 +0300

    feat(frontend):  add MultiShop.WebUI MVC web app in Frontend folder

---
+++ MultiShop/Frontend/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/UILayoutFooterViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class UILayoutFooterViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
