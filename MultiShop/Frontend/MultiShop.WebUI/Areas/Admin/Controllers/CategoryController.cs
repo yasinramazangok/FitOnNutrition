ca5caa515b6417832513b35e8ac30002eba42490
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-03-18 11:11:37 +0300

    feat(frontend): add Admin template in MultiShop.WebUI project

---
+++ MultiShop/Frontend/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0= "Kategori İşlemleri";
+            return View();
+        }
+    }
+}
