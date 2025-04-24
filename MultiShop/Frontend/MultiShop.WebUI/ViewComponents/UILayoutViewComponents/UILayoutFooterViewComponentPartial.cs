6e51c8b8be86d1e2d00708a8466a533b3299f1f2
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-04-24 22:23:51 +0300

    feat(application): add About operations in MultiShop.WebUI and MultiShop.Catalog projects

--- MultiShop/Frontend/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/UILayoutFooterViewComponentPartial.cs
+++ MultiShop/Frontend/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/UILayoutFooterViewComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.Dtos.AboutDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class UILayoutFooterViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UILayoutFooterViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Abouts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var result = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(result);
+                return View(values);
+            }
             return View();
         }
     }
