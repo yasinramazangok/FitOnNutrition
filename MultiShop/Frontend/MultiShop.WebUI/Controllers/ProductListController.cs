7e6fcaf597e0ec76bf8ae68bc924dd4a53ac5b94
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-05-08 21:28:34 +0300

    feat(frontend): update Comment operations in MultiShop.WebUI

--- MultiShop/Frontend/MultiShop.WebUI/Controllers/ProductListController.cs
+++ MultiShop/Frontend/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,18 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.Dtos.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index(string id)
         {
-             ViewBag.i = id;
+            ViewBag.i = id;
             return View();
         }
 
         public IActionResult ProductDetail(string id)
         {
             ViewBag.j = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateUserCommentDto createUserCommentDto)
+        {
+            createUserCommentDto.ImageUrl = "test.png";
+            createUserCommentDto.Rating = 1;
+            createUserCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createUserCommentDto.IsApproved = false;
+            createUserCommentDto.ProductId = "6807ec42a2192446d9862d73";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createUserCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7075/api/UserComments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
