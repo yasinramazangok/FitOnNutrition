c2c37375168b193b5c50cd8cde78428474c90971
yasinramazangok <yasinrmzngok@gmail.com>
2025-06-08 20:11:38 +0300

    feat(backend): add RabbitMQ functionality to MessagesController - Implemented CreateMessage and ReadMessage methods for publishing and consuming messages using RabbitMQ. - Updated project file to include RabbitMQ.Client package reference.

--- MultiShop/Services/RabbitMQ/MultiShop.RabbitMQWebApi/Controllers/MessagesController.cs
+++ MultiShop/Services/RabbitMQ/MultiShop.RabbitMQWebApi/Controllers/MessagesController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace MultiShop.RabbitMQWebApi.Controllers
 {
@@ -7,10 +10,60 @@ namespace MultiShop.RabbitMQWebApi.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        [HttpGet]
+        [HttpPost]
         public IActionResult CreateMessage()
         {
-            return Ok("RabbitMQ Web API is running!");
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("Queue2", false, false, false, arguments: null);
+
+            var messageContent = "Merhaba, bugün hava çok soğuk!";
+
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+
+            channel.BasicPublish(exchange: "", routingKey: "Queue2", basicProperties: null, body: byteMessageContent);
+
+            return Ok("Mesajınız alındı!");
+        }
+
+        private static string message;
+
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, x) =>
+            {
+                var byteMessage = x.Body.ToArray();
+                message = Encoding.UTF8.GetString(byteMessage);
+            };
+
+            channel.BasicConsume(queue: "Queue2", autoAck: false, consumer: consumer);
+
+            if (string.IsNullOrEmpty(message))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(message);
+            }
         }
     }
 }
